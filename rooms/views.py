18ffbf33d4376509ebf622a5a861d8b7506dd4d8
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-13 19:57:30 -0800

    Add files via upload

---
+++ rooms/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import Room, Door
+
+from .serializers import *
+
+@api_view(['GET', 'POST'])
+def rooms_list(request):
+    if request.method == 'GET':
+        data = Room.objects.all()
+
+        serializer = RoomSerializer(data, context={'request': request}, many = True)
+
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = RoomSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(esrializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def rooms_detail(request. pk):
+    try:
+        room = Room.objects.get(pk=pk)
+    except Room.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'PUT':
+        serializer = RoomSerializer(room, data=request.data,context={'request':request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        room.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
