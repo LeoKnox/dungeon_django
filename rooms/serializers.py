ed1e8e804307fc0e7e1ed039aa3041f72a2744e7
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-14 21:30:33 -0800

    Add files via upload

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from .models import Room, Door
 
-class RoomSerializer(serializers.ModelSerializer):
-    #doors = serializers.RelatedField(queryset = Door.objects.all(), many=True)
-    doors = serializers.PrimaryKeyRelatedField(queryset = Door.objects.all(), many=True)
-    class Meta:
-        model = Room
-        fields = ('pk', 'name', 'description', 'floor', 'width', 'length', 'doors')
-
 class DoorSerializer(serializers.ModelSerializer):
-    rooms = RoomSerializer(many = True, read_only = True)
     class Meta:
         model = Door
         fields = ('pk', 'room_a', 'room_b', 'rooms')
+
+class RoomSerializer(serializers.ModelSerializer):
+    #doors = serializers.RelatedField(queryset = Door.objects.all(), many=True)
+    #doors = serializers.PrimaryKeyRelatedField(queryset = Door.objects.all(), many=True)
+    doors = DoorSerializer(many = True, read_only=True)
+    class Meta:
+        model = Room
+        fields = ('pk', 'name', 'description', 'floor', 'width', 'length', 'doors')
