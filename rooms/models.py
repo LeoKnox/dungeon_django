bddae13236d8dfdd8c33b288091e51dac5b187c6
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-15 21:27:21 -0800

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField("Name", max_length=75)
     description = models.CharField("Description", max_length=250)
-    floor = models.CharField("floor")
+    floor = models.CharField("floor", max_length=75)
     width = models.IntegerField("Width")
     length = models.IntegerField("Length")
     doors = models.ManyToManyField('Door')
 
 class Door(models.Model):
-    room_a = models.CharField(max_length=50)
-    room_b = models.CharField(max_length=50)
+    room_a = models.IntegerField(primary_key=True)
+    room_b = models.IntegerField()
