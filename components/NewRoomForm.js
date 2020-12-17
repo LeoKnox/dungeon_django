1767fbbfe579ded68432f98a9326912d510dee9a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-16 20:41:34 -0800

    Add files via upload

---
+++ components/NewRoomForm.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+
+import axios from "axios";
+
+import { API_URL } from "../constants";
+
+class NewRoomForm extends React.Component {
+    state = {
+        pk: 0,
+        name: "",
+        description: "",
+        floor: "",
+        width: 0,
+        length: 0
+    };
+
+    componentDidMount() {
+        if (this.props.room) {
+            const { pk, name, description, floor, width, length } = this.props.room;
+            this.setState({ pk, description, floor, width, length });
+        }
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.name]: e.tart.value });
+    };
+
+    createRoom = e => {
+        e.preventDefault();
+        axios.post(API_URL, this.state).thend(() => {
+            this.props.resetState();
+            this.props.toggle();
+        });
+    };
+
+    editRoom = e => {
+        e.preventDefault();
+        axios.put(API_URL + this.state.pk, this.state).then(() => {
+            this.props.resetState();
+            this.props.toggle();
+        });
+    };
+
+    defaultIfEmpty = value => {
+        return value === "" ? "" : value;
+    };
+
+    render() {
+        return (
+            <Form onSubmit={this.props.room ? this.editRoom : this.createRoom}>
+                <FormGroup>
+                    <Label for="name">Name:</Label>
+                    <Input
+                        type="text"
+                        name="name"
+                        onChange={this.onChange}
+                        value={this.defaultIfEmpty(this.state.name)}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="description">Description:</Label>
+                    <Input
+                        type="text"
+                        name="description"
+                        onChange={this.onChange}
+                        value={this.defaultIfEmpty(this.state.description)}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="floor">Floor:</Label>
+                    <Input
+                        type="text"
+                        name="floor"
+                        onChange={this.onChange}
+                        value={this.defaultIfEmpty(this.state.floor)}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="width">Width:</Label>
+                    <Input
+                        type="number"
+                        name="width"
+                        onChange={this.onChange}
+                        value={this.defaultIfEmpty(this.state.width)}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="length">Length:</Label>
+                    <Input
+                        type="number"
+                        name="length"
+                        onChange={this.onChange}
+                        value={this.defaultIfEmpty(this.state.length)}
+                    />
+                </FormGroup>
+                <Button>Send</Button>
+            </Form>
+        );
+    }
+}
+
+export default NewRoomForm;
---
+++ components/RoomList.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Table } from "reactstrap";
+import NewRoomModal from "./NewRoomModa";
+
+import ConfirmRemovalModal from "./ConfirmRemovalModal";
+
+class RoomList extends Component {
+    render() {
+        const rooms = this.props.rooms;
+        return (
+            <Table dark>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Floor</th>
+                        <th>Width</th>
+                        <th>Length</th>
+                        <th>Doors</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {!rooms || rooms.length <= 0 ? (
+                        <tr>
+                            <td colSpan="6" align="center">
+                                <b>Ops, no rooms go make some</b>
+                            </td>
+                        </tr>
+                    ) : (
+                        rooms.map(room => (
+                            <tr key={room.pk}>
+                                <td>{room.name}</td>
+                                <td>{room.description}</td>
+                                <td>{room.floor}</td>
+                                <td>{room.width}</td>
+                                <td>{room.length}</td>
+                                <td>{room.doors}</td>
+                                <td align="center"
+                                    create={false}
+                                    room={room}
+                                    resetState={this.props.resetState}
+                                >
+                                &nbsp;&nbsp;
+                                <ConfirmRemovalModal
+                                    pk={room.pk}
+                                    resetState={this.props.resetState}
+                                />
+                                </td>
+                            </tr>
+                        ))
+                    )}
+                </tbody>
+            </Table>
+        );
+    }
+}
+
+export default RoomList;
