71d021df49f4e00a986ce272577f65ff29f84091
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-22 20:37:20 -0800

    Add files via upload

---
+++ rooms-fe/src/components/SingleRoomModal.js
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from "react";
+import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
+
+class SingleRoomModal extends Component {
+    state = {
+        modal: false
+    };
+
+    toggle = () => {
+        this.setState(previous => ({
+            modal: !previous.modal
+        }));
+    };
+
+    render() {
+        const create = this.props.create;
+
+        var title = "Your Room";
+
+        return (
+            <Fragment>
+                <p>Blue</p>
+            </Fragment>
+        )
+    }
+}
