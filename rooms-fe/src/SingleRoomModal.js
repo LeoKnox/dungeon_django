159e05d781c9df4f366293aa423992d3f0b2bddd
LeoKnox <LeoKnox@users.noreply.github.com>
2020-12-28 21:35:55 -0800

    Add files via upload

---
+++ rooms-fe/src/SingleRoomModal.js
@@ -0,0 +1,52 @@
+import React, { Component, Fragment } from "react";
+import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
+import SingleRoomDisplay from "./SingleRoomDisplay"
+
+class SingleRoomModal extends Component {
+    state = {
+        modal: false
+    };
+
+    toggle = () => {
+        this.setState(previous => ({
+            modal: !previous.modal
+        }));
+    };
+
+    render() {
+        const create = this.props.create;
+
+        var title = "Your Room";
+
+        var button = (
+            <Button
+                color="info"
+                className="float-right"
+                onClick={this.toggle}
+                style={{ minWidth: "100px" }}
+            >
+                Room SRM {create}
+            </Button>
+        )
+
+        return (
+            <Fragment>
+                {button}
+
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                    <ModalHeader toggle={this.toggle}>{title}</ModalHeader>
+
+                    <ModalBody>
+                        <SingleRoomDisplay
+                            resetState={this.props.resetState}
+                            toggle={this.toggle}
+                            room={this.props.room}
+                        />
+                    </ModalBody>
+                </Modal>
+            </Fragment>
+        );
+    }
+}
+
+export default SingleRoomModal;
